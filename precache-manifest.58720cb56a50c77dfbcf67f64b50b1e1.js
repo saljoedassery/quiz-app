e78b478d40c5425283bff648293b07b4b7adaa90
Saljo <saljo@dexlock.com>
2020-04-28 18:28:29 +0530

    Updates

---
+++ precache-manifest.58720cb56a50c77dfbcf67f64b50b1e1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b38ba935b6c1d67f1ec36e276f821a8f",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "1d6c9690efa1708cfedf",
+    "url": "/quiz-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c8e9b0a4b96bf214e09a",
+    "url": "/quiz-app/static/css/main.d90ef630.chunk.css"
+  },
+  {
+    "revision": "1d6c9690efa1708cfedf",
+    "url": "/quiz-app/static/js/2.d657be87.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/quiz-app/static/js/2.d657be87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e9b0a4b96bf214e09a",
+    "url": "/quiz-app/static/js/main.d5e7fb66.chunk.js"
+  },
+  {
+    "revision": "b361294a09da10485cc7",
+    "url": "/quiz-app/static/js/runtime-main.bd5e6a93.js"
+  },
+  {
+    "revision": "4f6abd89a2bcd9c67f503dd82b1fe268",
+    "url": "/quiz-app/static/media/alert.4f6abd89.svg"
+  },
+  {
+    "revision": "0e3ae76b14a328e33bb0be4d9f7a664a",
+    "url": "/quiz-app/static/media/back.0e3ae76b.svg"
+  },
+  {
+    "revision": "d341d7008489d766e3713aac7317eee0",
+    "url": "/quiz-app/static/media/next.d341d700.svg"
+  }
+]);
